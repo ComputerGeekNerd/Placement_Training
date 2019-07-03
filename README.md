410842430530b1a905af5d6a508074008d715c58
ComputerGeekNerd <rakshaaasish@gmail.com>
2019-07-03 14:09:00 +0530

    Day 2 training

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Questions and codes taught in summer placement training
 Q1- Given string of numver sequences. Assuming A=1, B=2..Z=26,find the number of ways in which the given sequnce can be encoded into string characters. 
 (enc.py)
 
+Q2 - Given an array of integers and a value'k'. Find the combination of any 4 numbers in the array whose sum = k.  
---
+++ four_comb.py
@@ -0,0 +1,32 @@
+#Exact implementation of itertools combinations function
+
+def combin(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = list(range(r))
+    #yield tuple(pool[i] for i in indices)
+    yield tuple((tuple(pool[i] for i in indices),tuple(indices)))
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        #yield tuple(pool[i] for i in indices) #Returns tuple of combination elements
+        yield tuple((tuple(pool[i] for i in indices),tuple(indices)))
+
+arr = [int(x) for x in input("Enter array:").split()]
+r = int(input("No. of combinations:"))
+k = int(input("Enter sum:"))
+com = combin(arr,r)
+for i in com:
+     if sum(i[0]) == k:
+          print(i[0],end=':')
+          print(i[1])
