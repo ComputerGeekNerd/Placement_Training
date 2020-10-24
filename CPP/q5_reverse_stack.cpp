f7f3174f14a6cfc5bbfab088ac3b2ab5dee96c85
Raksha Aasish <raksha.aasish@juspay.in>
2020-10-24 22:26:31 +0530

    Modifying the way the stack is being printed

--- CPP/q5_reverse_stack.cpp
+++ CPP/q5_reverse_stack.cpp
@@ -15,12 +15,13 @@ int main ()
         cin >> val;
         s1.push(val);
     }
-    cout << "\n\nValues being pushed into the reverse stack:\n";
     //Pop values from stack1 and push it into stack2. This way
     //the values will be stored into stack 2 in reverse order.
     for (int i = 0; i < n; i++) {
         val = s1.pop();
-        cout << val << endl;
         s2.push(val);
     }
+    cout << "The stack is : "; 
+    showstack(s2);
+    cout << "\nThe top is : " << s2.top();
 }
